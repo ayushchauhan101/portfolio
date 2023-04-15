ee995d5a189323e521a02c50259053dc3722a4b3
ayush <ayush@email.com>
2023-04-16 00:11:45 +0530

    reworking mobile dropdown lists

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import JumpTo from "./components/JumpTo"
 function App() {
 	return (
 		// global container
-		<div className="main  dark:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+		<div className="main  from-indigo-500 via-purple-500 to-pink-500 dark:bg-gradient-to-r">
 			<div className="mx-4 flex flex-col space-y-8 bg-white/50 dark:bg-slate-800/80">
 				<div className="m-2 mb-4 flex flex-col justify-between space-x-0 space-y-8 rounded-md p-1 md:m-8 md:mb-16 md:space-y-16 md:p-4">
 					<Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,18 @@
-import MenuBar from "./HeaderComponents/Example"
-
-function HeaderButton(props) {
-	return <button className="header-btn">{props.children}</button>
-}
+import MenuBar from "./HeaderComponents/MenuBar"
 
 function Header() {
-	function clickHandler(e) {}
 	return (
 		<header>
 			<div className="header-div">
+				{/* Title */}
 				<div>
 					<section id="home">
 						<h1>Pragma</h1>
 						<p className="text-center">Built on react + vite + tailwind</p>
 					</section>
 				</div>
-				<div className="flex flex-col items-center md:invisible">
-					<HeaderButton>
-						<a href="#profile" onClick={clickHandler}>
-							Go to Profile
-						</a>
-					</HeaderButton>
-					<HeaderButton>
-						<a href="#skills" onClick={clickHandler}>
-							Go to Skills
-						</a>
-					</HeaderButton>
-					<HeaderButton>
-						<a href="#projects" onClick={clickHandler}>
-							Go to Projects
-						</a>
-					</HeaderButton>
-					<HeaderButton>
-						<a href="#footer" onClick={clickHandler}>
-							Go to Footer
-						</a>
-					</HeaderButton>
-				</div>
-				<div className="invisible md:visible">
+				{/* Dropdown Menu */}
+				<div className="collapse md:visible">
 					<MenuBar />
 				</div>
 			</div>
