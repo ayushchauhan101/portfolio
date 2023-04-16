0402d285112d366e2e19e94e1f0fa11272373c12
ayush <ayush@email.com>
2023-04-16 12:13:54 +0530

    improved dark mode support

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import JumpTo from "./components/JumpTo"
 function App() {
 	return (
 		// global container
-		<div className="main  from-indigo-500 via-purple-500 to-pink-500 dark:bg-gradient-to-r">
-			<div className="mx-auto flex flex-col space-y-8 bg-white/50 dark:bg-slate-800/80 md:max-w-5xl">
+		<div className="main">
+			<div className="background">
 				<div className="m-2 mb-4 flex flex-col justify-between space-x-0 space-y-8 rounded-md p-1 md:m-8 md:mb-16 md:space-y-16 md:p-4">
 					<Header />
 					<JumpTo />
